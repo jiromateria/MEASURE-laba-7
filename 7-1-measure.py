3b04f3b5fd82017c9c8e01d87ffb1e1ffeb867c5
jiromateria <kolyasorokin2018@mail.ru>
2025-03-22 05:57:14 +0300

    Update 7-1-measure.py

--- 7-1-measure.py
+++ 7-1-measure.py
@@ -1,82 +1,105 @@
 import RPi.GPIO as gpio
-import matplotlib.pyplot as plt
 import time
+import matplotlib.pyplot as plt
 
-def dec2bin(value):
-    return [int(i) for i in bin(value)[2:].zfill(8)]
+def adc( dac, razr ):
+    data = [0, 0, 0, 0, 0, 0, 0, 0 ]
 
-def dac_leds(value):
-    signal = dec2bin(value)
-    gpio.output( dac, signal)
-    return signal
+    for i in range(razr):
+        data[i] = 1
+        gpio.output(dac, data)
 
-def getVal():
-    lvl = 0
-    for i in range(bits - 1, -1, -1):
-        lvl += 2**i
-        gpio.output(dac, dec2bin(lvl))
-        time.sleep(0.01)
-        comp_value  = gpio.input(comp)
-        if (comp_value == 0):
-            lvl -= 2**i
-    return lvl
+        time.sleep(0.002)
+        if gpio.input(comp):
+            data[i] = 0
+
+        gpio.output(dac, data)
+
+    return bin_list_2_dec(data)
+
+def dec2bin( value ):
+
+    ans = [int(i) for i in bin( value)[2:]]
+    while len(ans) < 8:
+        ans.insert(0, 0)
+
+    return ans
+
+def bin_list_2_dec(data):
+    s = ""
+    for i in data:
+        s += str(i)
+    return int(s, 2)
 
 
-dac = [ 8, 11, 7, 1, 0, 5, 12, 6 ]
+dac = [8, 11, 7, 1, 0, 5, 12, 6]
+led = [2, 3, 4, 17, 27, 22, 10, 9]
 troyka = 13
 comp = 14
-bits = len(dac)
-lvls = 2 ** bits
-max_volt = 3.3
+maxV = 3.3
+razr = 8
 
 gpio.setmode(gpio.BCM)
 
-gpio.setup(troyka, gpio.OUT, initial=gpio.LOW)
-gpio.setup(dac, gpio.OUT)
+gpio.setup(troyka, gpio.OUT)
 gpio.setup(comp, gpio.IN)
+gpio.setup(led, gpio.OUT)
+gpio.setup(dac, gpio.OUT)
 
-gpio.output(troyka, 0)
-
-volts_data = []
-times_data = []
+timeData = []
+voltData = []
 
 try:
-    start_time = time.time()
-    value = 0
+    start = time.time()
+    print("Zariadka")
     gpio.output(troyka, 1)
-    while(value < 248):
-        value = getVal()
-        dac_leds(value)
-        volts_data.append(value*max_volt/ lvls )
-        times_data.append(time.time() - start_time)
+    val = 0
+
+    while val < 240:
+        val = adc(dac, razr)
+        gpio.output(led, dec_2_bin_list(val))
+        valV = val * maxV / 256
+
+        print(val)
+        print(dec_2_bin_list(val))
+        print()
+
+        voltData.append(val)
+        timeData.append(time.time() - start)
 
     gpio.output(troyka, 0)
+    print("Razriadka")
 
-    while(value > 5):
-        value = getVal()
-        dac_leds(value)
-        volts_data.append(value*max_volt/ lvls )
-        times_data.append(time.time() - start_time)
+    while val > 10:
+        val = adc(dac, razr)
+        gpio.output(led, dec_2_bin_list(val))
+        valV = val * maxV / 256
 
-    end_time = time.time()
+        print(val)
+        print(dec_2_bin_list(val))
+        print()
 
-    with open("./settings.txt", "w") as file:
-        file.write(str((end_time - start_time) / len(volts_data)))
-        file.write(("\n"))
-        file.write(str(max_volt / 256))
+        voltData.append(val)
+        timeData.append(time.time() - start)
 
-    print(end_time - start_time, " secs\n", len(volts_data) / (end_time - start_time), "\n", max_volt / lvls)
+    end = time.time()
+
+
+    print("Продолжительность:", end - start)
+    print("Период одного измерения:", (end - start) / len(voltData))
+    print("Частота дискретизации:", len(voltData) / (end - start))
+    print("Шаг квантования АЦП:", maxV / (2**razr-1))
+
+    with open("experiment_data.txt", "w") as file:
+        for i in range(len(voltData)):
+            file.write(str(voltData[i]) + ' ')
+            file.write(str(timeData[i]) + '\n')
+
+
+    plt.scatter(timeData, voltData, s=1)
+    plt.show()
 
 finally:
-    gpio.output(dac, gpio.LOW)
-    gpio.output(troyka, gpio.LOW)
+    gpio.output(dac, 0)
+    gpio.output(led, 0)
     gpio.cleanup()
-
-times_data_str = [str(item) for item in times_data]
-volts_data_str = [str(item) for item in volts_data]
-
-with open("data.txt", "w") as file:
-    file.write("\n".join(volts_data_str))
-
-plt.plot(times_data, volts_data)
-plt.show()
